c7003acd485766305ee8abe71f1b4c4f013e9131
Rollin Morris <rollinm@gmail.com>
2019-07-19 16:20:36 -0500

    config update

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 okta = {
+<<<<<<< HEAD
     "org_host": "https://ironman.oktapreview.com", # Okta host org / tennant URL i.e. https://myoktaorg.okta.com
     "api_token": "api key here", # Okta API Token created for the server side to access Okta's API for your tenant
     "app_host": "http://18.222.141.236:8080", # The URL to access this application, makes redirects and helper urls easier to reference
+=======
+    "org_host": "https://xxxx.oktapreview.com", # Okta host org / tennant URL i.e. https://myoktaorg.okta.com
+    "api_token": "xxxxx", # Okta API Token created for the server side to access Okta's API for your tenant
+    "app_host": "http://xxxx:8080", # The URL to access this application, makes redirects and helper urls easier to reference
+>>>>>>> parent of b4943de... config updates
     "oidc_client_id": "", # OIDC Application created in Okta client id
     "oidc_client_secret": "", # OIDC Application created in Okta client secret
     "redirect_uri": "", # OIDC Redirect URI to handle auth code flow
